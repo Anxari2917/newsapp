03fa886e06a6c8885e93e3f86aa06d7ee0addaca
Anxari2917 <147482665+Anxari2917@users.noreply.github.com>
2023-10-17 22:56:29 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This is First Time in Class Basied Copmonent
+    </div>
+  );
+}
+
+export default App;
